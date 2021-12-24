9cde93fe42ff428a4d14aa3a753692e936d5f18e
Olagoke Oladokun <gokeladokun@hotmail.com>
2021-12-24 01:05:32 +0100

    Add market.d.html

---
+++ ecom-vue-app/src/components/Market.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+const pe
+</script>
+
+<template>
+    <h1>My Market</h1>
+
+</template>
--- gokPrj/data.js
+++ gokPrj/data.js
@@ -1,34 +1,34 @@
 products = [
-    {
-        id: 1,
-        name: "Milo 250",
-        description: "",
-        quantity: 5,
-        stores: [
-          { name: "Store 1", amount: 100.00 },
-          { name: "Store 2", amount: 110.00 },
-          { name: "Store 4", amount: 110.50 },
-        ],
-      },
-      {
-        id: 1,
-        name: "Pineapple",
-        description: "",
-        quantity: 5,
-        stores: [
-          { name: "Store 1", amount: 30.00 },
-          { name: "Store 2", amount: 110.00 },
-        ],
-      },
-      {
-        id: 1,
-        name: "Tomato",
-        description: "",
-        quantity: 5,
-        stores: [
-          { name: "Store 1", amount: 101.99 },
-          { name: "Store 2", amount: 110.00 },
-          { name: "Store 3", amount: 110.50 },
-        ],
-      },
+  {
+    id: 1,
+    name: "Milo 250",
+    description: "",
+    quantity: 5,
+    stores: [
+      { name: "Store 1", amount: 100.0 },
+      { name: "Store 2", amount: 110.0 },
+      { name: "Store 4", amount: 110.5 },
+    ],
+  },
+  {
+    id: 1,
+    name: "Pineapple",
+    description: "",
+    quantity: 5,
+    stores: [
+      { name: "Store 1", amount: 30.0 },
+      { name: "Store 2", amount: 110.0 },
+    ],
+  },
+  {
+    id: 1,
+    name: "Tomato",
+    description: "",
+    quantity: 5,
+    stores: [
+      { name: "Store 1", amount: 101.99 },
+      { name: "Store 2", amount: 110.0 },
+      { name: "Store 3", amount: 110.5 },
+    ],
+  },
 ];
