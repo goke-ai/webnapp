ab27e50bc58c4e299498821907569605e41981d1
Olagoke Oladokun <gokeladokun@hotmail.com>
2021-12-24 09:06:42 +0100

    Add market1

---
+++ gokPrj/market1.js
@@ -0,0 +1,50 @@
+const marketApp = {
+  data() {
+    return {
+      title: "My Market App",
+
+      products: [
+        {
+          id: 1,
+          name: "Milo 250",
+          description: "Beverage drink",
+          imageUrl: "./images/milo.jpg",
+          stores: [
+            { id: 1, name: "Store 1", quantity: 1, price: 10.0 },
+            { id: 2, name: "Store 2", quantity: 8, price: 100.0 },
+            { id: 4, name: "Store 4", quantity: 91, price: 1000.0 },
+          ],
+        },
+        {
+          id: 2,
+          name: "Pineapple",
+          description: "Fresh fruit drink with all the fibers.",
+          imageUrl: "./images/pineapple.jpg",
+          stores: [
+            { id: 1, name: "Store 1", quantity: 2, price: 10.0 },
+            { id: 2, name: "Store 2", quantity: 2.5, price: 10.0 },
+          ],
+        },
+        {
+          id: 3,
+          name: "Plate",
+          description: "Ceramic flat plate for serving food",
+          imageUrl: "./images/plate.jpg",
+          stores: [
+            { id: 1, name: "Store 1", quantity: 1, price: 10.0 },
+            { id: 2, name: "Store 2", quantity: 2, price: 10.0 },
+            { id: 3, name: "Store 3", quantity: 3, price: 10.0 },
+          ],
+        },
+      ],
+    };
+  },
+
+  methods: {
+    quantity(stores) {
+      return stores.reduce((T, q) => T + q.price, 0);
+    },
+  },
+};
+
+Vue.createApp(marketApp).mount("#app");
